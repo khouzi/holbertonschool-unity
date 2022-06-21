7e03950b96198b29f179496e98d4e15ecbd7e933
khouzi <khouzem.boussaid@gmail.com>
2022-06-21 11:49:19 +0200

    0x06-unity-assets_ui

---
+++ 0x06-unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void LevelSelect(int level)
+    {
+        SceneManager.LoadScene(level, LoadSceneMode.Single);
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene(4, LoadSceneMode.Single);
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(3, LoadSceneMode.Single);
+    }
+}
